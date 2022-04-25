1daa0703dbb0731f0c59fcf5812fa7ade3d9b09c
DimasVeliz <trevor90094@yandex.com>
2022-04-26 01:50:24 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SuvillianceSystem.RabbitMQAuthorizationService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
